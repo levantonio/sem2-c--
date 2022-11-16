662e03d93dbc90a4ce2995143599ad8a71782bd7
Anton <levantonio@mail.ru>
2022-11-16 11:48:54 +0300

    task 14

---
+++ task14/Program.cs
@@ -0,0 +1,21 @@
+﻿// 14. написать программу которая
+// 1. принимает на вход число
+// 2. проверяет кратно ли оно одновременно 7 и 23
+// 14 --> нет
+// 46 --> нет
+// 161 --> да
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool MultipLicityTwoDigits (int num)
+{
+    return num % 7 == 0 && num % 23 == 0;  // && - and 
+    
+}
+
+bool result = MultipLicityTwoDigits(number); //Выводит false/true
+
+string resultStr = result ? "да" : "нет"; //Выводит да/нет да == true*** нет == false тернарный орератор : == иначе
+
+Console.WriteLine($"{number} -> {resultStr}");
